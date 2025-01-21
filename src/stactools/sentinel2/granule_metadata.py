64f84ced40d73edc5991e53dd96c8f491b7fad80
Jonas Eberle <jonas.eberle@gmx.de>
2025-01-21 08:48:13 +0100

    Added Sentinel-2C as platform

--- src/stactools/sentinel2/granule_metadata.py
+++ src/stactools/sentinel2/granule_metadata.py
@@ -214,6 +214,8 @@ class GranuleMetadata:
             return "sentinel-2a"
         elif self.tile_id.startswith("S2B"):
             return "sentinel-2b"
+        elif self.tile_id.startswith("S2C"):
+            return "sentinel-2c"
         else:
             return None
 
